154eac517d87b0debd15b6a6bdc4233865110013
Tomohiro Kusumi <tkusumi@netbsd.org>
2023-05-24 00:12:09 -0700

    v1.1.0

--- src/sys/fs/hammer2/hammer2_io.c
+++ src/sys/fs/hammer2/hammer2_io.c
@@ -353,6 +353,20 @@ hammer2_io_data(hammer2_io_t *dio, off_t lbase)
 }
 
 int
+hammer2_io_new(hammer2_dev_t *hmp, int btype, off_t lbase, int lsize,
+    hammer2_io_t **diop)
+{
+	return (EOPNOTSUPP);
+}
+
+int
+hammer2_io_newnz(hammer2_dev_t *hmp, int btype, off_t lbase, int lsize,
+    hammer2_io_t **diop)
+{
+	return (EOPNOTSUPP);
+}
+
+int
 hammer2_io_bread(hammer2_dev_t *hmp, int btype, off_t lbase, int lsize,
     hammer2_io_t **diop)
 {
@@ -364,4 +378,9 @@ void
 hammer2_io_bqrelse(hammer2_io_t **diop)
 {
 	hammer2_io_putblk(diop);
+}
+
+void
+hammer2_io_dedup_set(hammer2_dev_t *hmp, hammer2_blockref_t *bref)
+{
 }
